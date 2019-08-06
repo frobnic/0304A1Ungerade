4b86c62196931bd9672885898f8d24a256606826
frank paulsen <frobnic+ita@gmail.com>
2019-08-06 10:34:45 +0200

    code inserted

--- 0304A1Ungerade/Program.cs
+++ 0304A1Ungerade/Program.cs
@@ -10,6 +10,22 @@ namespace _0304A1Ungerade
     {
         static void Main(string[] args)
         {
+            int z;
+
+            Console.Write("Bitte erfassen Sie eine Zahl im Bereich von 1 und 100 : ");
+            z = int.Parse(Console.ReadLine());
+
+            if (z < 1 || z > 100)
+            {
+                Console.WriteLine("Zahl {0} nicht im Bereich von 1 bis 100", z);
+            }
+            else
+            {
+                if (z % 2 == 0)
+                    Console.WriteLine("Zahl {0} ist gerade", z);
+                else
+                    Console.WriteLine("Zahl {0} ist ungerade", z);
+            }
         }
     }
 }
